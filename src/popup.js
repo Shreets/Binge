53dd8740175d720caf21c30d0263c3e370ada8a1
Shreets <shreeti.019@gmai;.com>
2020-04-25 18:44:49 +0545

    responsive

--- src/popup.js
+++ src/popup.js
@@ -3,18 +3,20 @@ import style from "./popup.module.css";
 
 const Popup = (props) => {
   return (
-    <div className={style.container}>
+    <div className={style.container_pop}>
       <p>
         <strong>Binge</strong> is a TV-series reccomendation application that
         gives you options that you can watch along with details related to those
         series.
-        <br /> The chatbox gives you the options of movies that you can select
-        from.
+        <br /> <br />
+        The chatbox gives you the options of movies that you can select from.
+        <br />
         <br />
         <button className={style.button}>NO👎</button> buttons are placed to
         make selections. Upon selection of this button, the options will keep
         coming on the chat box while the details are displayed on information
         box on the right.
+        <br />
         <br />
         Select the button <button className={style.button}>YES👍</button> when
         you make up your mind regarding the series.
